88bf3b3c686419a8e147f2657b6507867acf591a
Bogdan <garfield1333@gmail.com>
2017-03-27 13:52:18 +0300

    database connection

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <!DOCTYPE html>
+<?php
+include 'connection.php';
+echo '<br/>';
+function listProducts($conn) {
+    $sql = 'SELECT Name, Price, Description FROM products';
+    foreach ($conn->query($sql) as $row) {
+        print $row['Name'] . "\t";
+        print $row['Price'] . "\t";
+        print $row['Description'] . "\n";
+    }
+}
+listProducts($conn);
+?>
 <html>
 <head>
     <title>Online Shop</title>
