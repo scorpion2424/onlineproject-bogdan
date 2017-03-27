35839906346b576cdd553f69d50df9d3ffb1e421
Bogdan <garfield1333@gmail.com>
2017-03-27 15:25:41 +0300

    list products

--- index.php
+++ index.php
@@ -1,22 +1,41 @@
 <!DOCTYPE html>
+<html>
 <?php
 include 'connection.php';
 echo '<br/>';
-function listProducts($conn) {
-    $sql = 'SELECT Name, Price, Description FROM products';
-    foreach ($conn->query($sql) as $row) {
-        print $row['Name'] . "\t";
-        print $row['Price'] . "\t";
-        print $row['Description'] . "\n";
-    }
-}
-listProducts($conn);
 ?>
-<html>
 <head>
     <title>Online Shop</title>
+<link rel="stylesheet" type="text/css" href="theme.css">
 </head>
 <body>
+<?php
+function listProducts($conn) {
+
+    $sql = 'SELECT Image, Name,  Description, Price  FROM products';
+    foreach ($conn->query($sql) as $row) {
+        ?>
+        <tr>
+        <td>  <img class ="productImage" src="<?php print strip_tags($row['Image']); ?>"  </td>
+        <td> <?php print strip_tags($row['Name']) ?> </td>
+        <td> <?php print strip_tags($row['Description']) ?> </td>
+        <td> <?php print strip_tags($row['Price']) ?> </td>
+        <td> <button>Add to cart</button></td>
+        </tr>
+        <?php
+    }
+}
+?>
+<table>
+<tr>
+    <th>Image</th>
+    <th>Name</th>
+    <th>Description</th>
+    <th>Price</th>
+    <th></th>
+</tr>
+<?php listProducts($conn); ?>
+</table>
 </body>
 
 </html>
