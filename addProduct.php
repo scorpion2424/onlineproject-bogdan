8da15966666b656635352d967b4afe49d7df081e
Bogdan <garfield1333@gmail.com>
2017-03-31 16:12:04 +0300

    list products

--- addProduct.php
+++ addProduct.php
@@ -17,7 +17,6 @@ function addProduct($conn)
         <form id="editDetails" method="post" action="http://localhost:90/project-bogdan/addProductDone.php" enctype="multipart/form-data">
             <tr>
 
-<!--                <td>  <img class ="productImage" src="--><?php //print htmlentities("smartphone.jpg"); ?><!--"  </td>-->
                 <td><input type="file" name="image" id="fileToUpload"></td>
                 <td><input type="text" name="productName" placeholder="name..."><br></td>
                 <td><input type="text" name="description" placeholder="description..."><br></td>
@@ -45,9 +44,9 @@ function addProduct($conn)
         foreach ($conn->query($sql) as $row) {
             ?>
             <form id="editDetails" method="post"
-                  action="http://localhost:90/project-bogdan/editProductDone.php?product=<?php echo htmlentities($row['ID']) ?>">
+                  action="http://localhost:90/project-bogdan/editProductDone.php?product=<?php echo htmlentities($row['ID']) ?> " enctype="multipart/form-data">
                 <tr>
-                    <td><img class="productImage" src="images/<?php print htmlentities($row['Image']); ?>"</td>
+                    <td><div class="productImageWrapper"><span class="addNewImage"><input type="file" name="image" id="fileToUpload"></span><img class="productImage" src="images/<?php print htmlentities($row['Image']); ?>"</div></td>
                     <td><input type="text" name="productName" value="<?php echo htmlentities($row['Name']) ?>"><br></td>
                     <td><input type="text" name="description"
                                value="<?php echo htmlentities($row['Description']) ?>"><br></td>
@@ -66,7 +65,6 @@ function addProduct($conn)
     }
 
 ?>
-
     <?php
     if(isset($_GET['product'])) {
         editProduct($conn);
--- editProductDone.php
+++ editProductDone.php
@@ -2,11 +2,50 @@
 include 'connection.php';
 include 'checkLogin.php';
 include 'header.php';
+function addImage(){
+    if(isset($_FILES['image'])){
+        $errors= array();
+        $file_name = $_FILES['image']['name'];
+        $file_size =$_FILES['image']['size'];
+        $file_tmp =$_FILES['image']['tmp_name'];
+        $file_type=$_FILES['image']['type'];
+        $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+
+        $expensions= array("jpeg","jpg","png");
+
+        if(in_array($file_ext,$expensions)=== false){
+            $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+        }
+
+        if($file_size > 2097152){
+            $errors[]='File size must be less than 2 MB';
+        }
+
+        if(empty($errors)==true){
+            move_uploaded_file($file_tmp,"images/".$file_name);
+            echo "Success";
+        }else{
+            print_r($errors);
+        }
+    }
+}
 
 try {
-    $stmt = $conn->prepare("UPDATE `products` SET `name` = :productName , `description`=:productDescription ,
-                          `price`=:productPrice WHERE `ID` = :productID ");
+    addImage();
+
 //echo $_GET['product'],$_POST['productName'],$_POST['description'],$_POST['price'];
+    if(isset($_FILES['image'])) {
+        $productImage = $_FILES['image']['name'];
+        $stmt = $conn->prepare("UPDATE `products` SET `Image` = :productImage, `name` = :productName , `description`=:productDescription ,
+                          `price`=:productPrice WHERE `ID` = :productID ");
+        $stmt->bindParam(':productImage', $productImage);
+    }
+    elseif(!isset($_FILES['image'])){
+
+        $stmt = $conn->prepare("UPDATE `products` SET  `name` = :productName , `description`=:productDescription ,
+                          `price`=:productPrice WHERE `ID` = :productID ");
+    }
+
         $stmt->bindParam(':productID', strip_tags($_GET['product']));
         $stmt->bindParam(':productName', strip_tags($_POST["productName"]));
         $stmt->bindParam(':productDescription', strip_tags($_POST["description"]));
@@ -21,5 +60,6 @@ catch(PDOException $e)
 {
     echo "Error: " . $e->getMessage();
 }
-header('Location: http://localhost:90/project-bogdan/adminPage.php');
+//header('Location: http://localhost:90/project-bogdan/adminPage.php');
+//exit();
 ?>
