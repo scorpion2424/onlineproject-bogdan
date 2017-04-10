0b3028713c1a4356180ffe47e0c5f9218891e9cc
scorpion2424 <garfield1333@gmail.com>
2017-04-10 11:00:21 +0300

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'connection.php';
-include 'checkLogin.php';
-include 'header.php';
-session_unset();
-session_destroy();
-header('Location: http://localhost:90/project-bogdan/index.php');
-?>
